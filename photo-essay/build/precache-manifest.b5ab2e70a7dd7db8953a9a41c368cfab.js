02bd8ea8f28d09de2dbaa7209d65d8a1df72e632
ahmadkady <i-ahmad.kady@devrev.ai>
2022-09-26 16:37:22 -0400

    text and photos

---
+++ photo-essay/build/precache-manifest.b5ab2e70a7dd7db8953a9a41c368cfab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "7390c2e22e73f991823266ace185e497",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "43b6af030fcf91baf3f7",
+    "url": "/static/css/main.30ada897.chunk.css"
+  },
+  {
+    "revision": "43b6af030fcf91baf3f7",
+    "url": "/static/js/main.43b6af03.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "354e092f0aa2734f8f4c",
+    "url": "/static/js/vendors~main.354e092f.chunk.js"
+  }
+]);
